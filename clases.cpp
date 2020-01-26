a538de50a6955fa0b560878e18cc706ab4c9d6ce
yo-cleon <cleon.form@gmail.com>
2020-01-26 12:43:27 +0000

    Adding a constructor example

--- clases.cpp
+++ clases.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
 using namespace std;
 
+//Definition of a class
 class myCar {
   public:
     string marca;
     int kms;
 
+    //Definition of a method/function INSIDE a class
     void arrancar() {
       cout << "brum, brum \n";
-    }
+    };
+    //Declaration of a method/function defined OUTSIDE of a class
+    void frenar();
+};
 
+//Definition of a method/function OUTSIDE a class
+void myCar::frenar() {
+  cout << "Frenando el ritmo" << endl;
+}
+
+class myCar2 {
+public:
+  string marca;
+  int kms;
+
+  myCar2(string x, int y) {
+    marca = x;
+    kms = y;
+  }
+
+  //Definition of a method/function INSIDE a class
+  void arrancar() {
+    cout << "Este coche también acelera \n";
+  };
 };
 
 int main() {
@@ -21,16 +45,17 @@ int main() {
   cout << "\t" << car1.marca << endl;
   cout << "\t" << car1.kms << endl;
   car1.arrancar();
+  car1.frenar();
   cout << "******************\n";
 
   cout << "\n";
 
-  myCar car2;
-  car2.marca = "Hiunday";
-  car2.kms = 150000;
+  myCar2 car2("Hiundya", 15000);
   cout << "**** Mi coche ***\n";
   cout << "\t" << car2.marca << endl;
   cout << "\t" << car2.kms << endl;
+  car2.arrancar();
   cout << "******************\n";
   return 0;
+
 }
